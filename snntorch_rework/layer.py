8fda994eb598ed2066cd305c150b4c964eb4cd79
Yegor Men <yegormenovchshikov@gmail.com>
2025-10-21 15:39:28 +1100

    States are internalized, makes stuff way easier to manage, also training works

--- snntorch_rework/layer.py
+++ snntorch_rework/layer.py
@@ -92,6 +92,16 @@ class Synaptic(nn.Module):
 		self.beta = nn.Parameter(beta)
 		self.threshold = nn.Parameter(threshold)
 
+		self.zero_states()
+
+	def zero_states(self):
+		self.syn = None
+		self.mem = None
+
+	def detach_states(self):
+		self.syn = self.syn.detach()
+		self.mem = self.mem.detach()
+
 	def param_to_dim(self, parameter, dim: int):
 		if dim == 1:
 			return parameter
@@ -104,38 +114,109 @@ class Synaptic(nn.Module):
 
 		raise ValueError(f"Dimension {dim} not in (1, 2, 3, 4) for MLP or CNN batched or unbatched")
 
-	def forward(self, x, syn, mem):
+	def forward(self, x):
+		if self.syn is None:
+			self.syn = torch.zeros_like(x)
+		if self.mem is None:
+			self.mem = torch.zeros_like(x)
+
 		alpha = self.param_to_dim(nn.functional.sigmoid(self.alpha), x.dim())
 		beta = self.param_to_dim(nn.functional.sigmoid(self.beta), x.dim())
 		threshold = self.param_to_dim(nn.functional.softplus(self.threshold), x.dim())
 
-		syn1 = syn * alpha + x
-		mem1 = mem * beta + syn1
-		x1 = self.surrogate_function(mem1 - threshold)
-		mem1 = mem1 - x1 * threshold
+		self.syn = self.syn * alpha + x
+		self.mem = self.mem * beta + self.syn
+		out_spikes = self.surrogate_function(self.mem - threshold)
+		self.mem = self.mem - out_spikes * threshold
 
-		return x1, syn1, mem1
+		return out_spikes
 
 
 # ======================================================================================================================
+from tqdm import tqdm
 
 
-import torch, torch.nn as nn
+class SNN(nn.Module):
+	def __init__(self, n_in, n_hidden, n_out):
+		super().__init__()
 
-# choose atan surrogate if desired
-lif = Synaptic(16, alpha=0.9, beta=0.85, threshold=1.0,
-			   surrogate_function=atan_surrogate(alpha=2.0))
+		self.lin1 = nn.Linear(n_in, n_hidden)
+		self.lif1 = Synaptic(n_hidden, 0.5, 0.5, 1.0, atan_surrogate(2.0))
 
-# toy linear upstream
-lin = nn.Linear(10, 16)
-x = torch.randn(2, 10)  # batch of 2
-pre = lin(x)  # [B, F]
-syn = torch.zeros_like(pre)
-mem = torch.zeros_like(pre)
+		self.lin2 = nn.Linear(n_hidden, n_hidden)
+		self.lif2 = Synaptic(n_hidden, 0.5, 0.5, 1.0, atan_surrogate(2.0))
 
-spk, syn, mem = lif(pre, syn, mem)
-loss = spk.sum()
-loss.backward()
+		self.lin3 = nn.Linear(n_hidden, n_out)
+		self.lif3 = Synaptic(n_out, 0.5, 0.5, 1.0, atan_surrogate(2.0))
 
-print("lin.weight.grad:", lin.weight.grad is not None)
-print("lif.raw_alpha.grad:", lif.alpha.grad is not None)
+	def zero_states(self):
+		self.lif1.zero_states()
+		self.lif2.zero_states()
+		self.lif3.zero_states()
+
+	def detach_states(self):
+		self.lif1.detach_states()
+		self.lif2.detach_states()
+		self.lif3.detach_states()
+
+	def forward(self, x):
+		x = self.lin1(x)
+		x = self.lif1(x)
+
+		x = self.lin2(x)
+		x = self.lif2(x)
+
+		x = self.lin3(x)
+		x = self.lif3(x)
+
+		return x
+
+
+n_in = 10
+n_hidden = 32
+n_out = 10
+
+rand_in = torch.rand(n_in)
+rand_out = torch.rand(n_out).round_()
+
+model = SNN(n_in, n_hidden, n_out)
+
+optimizer = torch.optim.AdamW(model.parameters())
+
+num_timesteps = 100
+num_epochs = 100
+
+losses = []
+
+for e in tqdm(range(num_epochs), total=num_epochs):
+	model.zero_states()
+	model.zero_grad()
+
+	average_output = torch.zeros(n_out)
+
+	for t in range(num_timesteps):
+		spike_input = torch.bernoulli(rand_in)
+		model_out = model(spike_input)
+		average_output = average_output + model_out
+
+	average_output = average_output / num_timesteps
+	loss = nn.functional.mse_loss(average_output, rand_out)
+	loss.backward()
+	losses.append(loss.item())
+	optimizer.step()
+
+import matplotlib.pyplot as plt
+
+plt.plot(losses, label="Loss")
+plt.legend()
+plt.title("Loss")
+plt.show()
+
+with torch.no_grad():
+	for t in tqdm(range(num_timesteps), total=num_timesteps):
+		spike_input = torch.bernoulli(rand_in)
+		model_out = model(spike_input)
+
+		loss = nn.functional.mse_loss(model_out, rand_out)
+
+		print(f"S: {model_out}, L: {loss.item()}")
