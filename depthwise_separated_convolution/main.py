ff3d8e042e8b1887994e1ae2a507ecad37e2d548
= <=>
2025-12-16 22:02:02 +1100

    Renamed the "low rank" to "depthwise", since I actually plan to use the code for SIID

--- depthwise_separated_convolution/main.py
+++ depthwise_separated_convolution/main.py
@@ -42,7 +42,7 @@ train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True
 test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
 
-class LowRankConv2d(nn.Module):
+class DSConv2d(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, kernel_size: int, depth_mul=1):
         super().__init__()
 
@@ -72,16 +72,16 @@ base_model = nn.Sequential(
     nn.Softmax(-1)
 ).to(device)
 
-low_rank_model = nn.Sequential(
-    LowRankConv2d(1, 16, 5),
+ds_model = nn.Sequential(
+    DSConv2d(1, 16, 5),
     nn.SiLU(),
-    LowRankConv2d(16, 32, 5),
+    DSConv2d(16, 32, 5),
     nn.SiLU(),
     nn.MaxPool2d(2, 2),
-    LowRankConv2d(32, 64, 3),
+    DSConv2d(32, 64, 3),
     nn.SiLU(),
     nn.MaxPool2d(2, 2),  # 8/2=4
-    LowRankConv2d(64, 128, 4),
+    DSConv2d(64, 128, 4),
     nn.Flatten(),
     nn.Linear(128, 128),
     nn.SiLU(),
@@ -96,15 +96,15 @@ def print_trainable_parameters(model, name):
 
 
 print_trainable_parameters(base_model, "BASE")
-print_trainable_parameters(low_rank_model, "LOW RANK")
+print_trainable_parameters(ds_model, "DEPTHWISE SEPARATED")
 
 base_optimizer = torch.optim.AdamW(base_model.parameters(), 1e-3)
-low_rank_optimizer = torch.optim.AdamW(low_rank_model.parameters(), 1e-3)
+ds_optimizer = torch.optim.AdamW(ds_model.parameters(), 1e-3)
 
 NUM_EPOCHS = 1
 
-base_losses, low_rank_losses = [], []
-smooth_base_losses, smooth_low_rank_losses = [0], [0]
+base_losses, ds_losses = [], []
+smooth_base_losses, smooth_ds_losses = [0], [0]
 loss_smoothness = 0.99
 
 from tqdm import tqdm
@@ -112,65 +112,64 @@ import matplotlib.pyplot as plt
 
 for e in range(NUM_EPOCHS):
     base_model.train()
-    low_rank_model.train()
+    ds_model.train()
 
     for image, label in tqdm(train_dataloader, total=len(train_dataloader), desc=f"TRAIN - E{e}"):
         image, label = image.to(device), label.to(device)
 
-        classic_output = base_model(image)
-        euclid_output = low_rank_model(image)
+        base_output = base_model(image)
+        ds_output = ds_model(image)
 
-        base_loss = nn.functional.mse_loss(classic_output, label)
-        low_rank_loss = nn.functional.mse_loss(euclid_output, label)
+        base_loss = nn.functional.mse_loss(base_output, label)
+        ds_loss = nn.functional.mse_loss(ds_output, label)
 
         base_losses.append(base_loss.item())
-        low_rank_losses.append(low_rank_loss.item())
+        ds_losses.append(ds_loss.item())
 
         smooth_base_losses.append(smooth_base_losses[-1] * loss_smoothness + base_loss.item() * (1 - loss_smoothness))
-        smooth_low_rank_losses.append(
-            smooth_low_rank_losses[-1] * loss_smoothness + low_rank_loss.item() * (1 - loss_smoothness))
+        smooth_ds_losses.append(smooth_ds_losses[-1] * loss_smoothness + ds_loss.item() * (1 - loss_smoothness))
 
         base_loss.backward()
-        low_rank_loss.backward()
+        ds_loss.backward()
 
         base_optimizer.step()
-        low_rank_optimizer.step()
+        ds_optimizer.step()
 
         base_model.zero_grad()
-        low_rank_model.zero_grad()
+        ds_model.zero_grad()
 
     plt.title("RAW")
     plt.plot(base_losses, label="Base")
-    plt.plot(low_rank_losses, label="Low Rank")
+    plt.plot(ds_losses, label="Depthwise Separated")
     plt.legend()
     plt.show()
 
     plt.title("SMOOTH")
     plt.plot(smooth_base_losses[1::], label="Base")
-    plt.plot(smooth_low_rank_losses[1::], label="Low Rank")
+    plt.plot(smooth_ds_losses[1::], label="Depthwise Separated")
     plt.legend()
     plt.show()
 
     base_model.eval()
-    low_rank_model.eval()
+    ds_model.eval()
 
     base_test_loss = 0
-    low_rank_test_loss = 0
+    ds_test_loss = 0
 
     with torch.no_grad():
         for image, label in tqdm(test_dataloader, total=len(test_dataloader), desc=f"TEST - E{e}"):
             image, label = image.to(device), label.to(device)
 
-            classic_output = base_model(image)
-            euclid_output = low_rank_model(image)
+            base_output = base_model(image)
+            ds_output = ds_model(image)
 
-            base_loss = nn.functional.mse_loss(classic_output, label)
-            low_rank_loss = nn.functional.mse_loss(euclid_output, label)
+            base_loss = nn.functional.mse_loss(base_output, label)
+            ds_loss = nn.functional.mse_loss(ds_output, label)
 
             base_test_loss += base_loss.item()
-            low_rank_test_loss += low_rank_loss.item()
+            ds_test_loss += ds_loss.item()
 
         base_test_loss /= len(test_dataloader)
-        low_rank_test_loss /= len(test_dataloader)
+        ds_test_loss /= len(test_dataloader)
 
-    print(f"BASE: {base_test_loss}, LOW RANK: {low_rank_test_loss}")
+    print(f"BASE: {base_test_loss}, DS: {ds_test_loss}")
